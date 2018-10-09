84adae1d6729a1508efe0204b5e9a03c5f3c7499
Haley Lebold <leboldh@flip3.engr.oregonstate.edu>
2018-10-09 15:29:54 -0700

    Added getShipType and setOccupiedSquares and relevant tests

--- src/main/java/cs361/battleships/models/Ship.java
+++ src/main/java/cs361/battleships/models/Ship.java
@@ -18,7 +18,63 @@ public class Ship {
 		occupiedSquares = new ArrayList<>();
 	}
 
+	public String getShipType(){
+		return shipType;
+	}
+
 	public List<Square> getOccupiedSquares() {
 		return occupiedSquares;
+	}
+
+	public void setOccupiedSquares(int x, char y, boolean isVertical){
+		switch(getShipType()){
+			case "MINESWEEPER":
+				if(isVertical){
+					for (int z = 0; z < 2; z++){
+						Square sq = new Square(x,y);
+						occupiedSquares.add(sq);
+						x++;
+					}
+				} else {
+					for (int z = 0; z < 2; z++){
+						Square sq = new Square(x,y);
+						occupiedSquares.add(sq);
+						y++;
+					}
+				}
+				break;
+			case "DESTROYER":
+				if(isVertical){
+					for (int z = 0; z < 3; z++){
+						Square sq = new Square(x,y);
+						occupiedSquares.add(sq);
+						x++;
+					}
+				} else {
+					for (int z = 0; z < 3; z++){
+						Square sq = new Square(x,y);
+						occupiedSquares.add(sq);
+						y++;
+					}
+				}
+				break;
+			case "BATTLESHIP":
+				if(isVertical){
+					for (int z = 0; z < 4; z++){
+						Square sq = new Square(x,y);
+						occupiedSquares.add(sq);
+						x++;
+					}
+				} else {
+					for (int z = 0; z < 4; z++){
+						Square sq = new Square(x,y);
+						occupiedSquares.add(sq);
+						y++;
+					}
+				}
+				break;
+			default:
+				break;
+		}
 	}
 }
--- src/test/java/cs361/battleships/models/ShipTest.java
+++ src/test/java/cs361/battleships/models/ShipTest.java
@@ -3,6 +3,7 @@ package cs361.battleships.models;
 import org.junit.Test;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -42,5 +43,78 @@ public class ShipTest {
     public void testGetOccupiedSquares_BlankList_StringConstructor(){
         Ship minesweeper = new Ship("MINESWEEPER");
         assertTrue(minesweeper.getOccupiedSquares().isEmpty());
+    }
+
+    @Test
+    public void testSetOccupiedSquares_MINESWEEPER_Vertical(){
+        Ship minesweeper = new Ship("MINESWEEPER");
+        minesweeper.setOccupiedSquares(2,'B',true);
+        assertEquals(minesweeper.getOccupiedSquares().get(0).getColumn(),'B');
+        assertEquals(minesweeper.getOccupiedSquares().get(0).getRow(),2);
+        assertEquals(minesweeper.getOccupiedSquares().get(1).getColumn(),'B');
+        assertEquals(minesweeper.getOccupiedSquares().get(1).getRow(),3);
+        assertEquals(minesweeper.getOccupiedSquares().size(),2);
+    }
+
+    @Test
+    public void testSetOccupiedSquares_MINESWEEPER_Horizontal(){
+        Ship minesweeper = new Ship("MINESWEEPER");
+        minesweeper.setOccupiedSquares(2,'B',false);
+        assertEquals(minesweeper.getOccupiedSquares().get(0).getColumn(),'B');
+        assertEquals(minesweeper.getOccupiedSquares().get(0).getRow(),2);
+        assertEquals(minesweeper.getOccupiedSquares().get(1).getColumn(),'C');
+        assertEquals(minesweeper.getOccupiedSquares().get(1).getRow(),2);
+        assertEquals(minesweeper.getOccupiedSquares().size(),2);
+    }
+
+    @Test
+    public void testSetOccupiedSquares_DESTROYER_Vertical(){
+        Ship destroyer = new Ship("DESTROYER");
+        destroyer.setOccupiedSquares(2,'B',true);
+        assertEquals(destroyer.getOccupiedSquares().get(0).getColumn(),'B');
+        assertEquals(destroyer.getOccupiedSquares().get(0).getRow(),2);
+        assertEquals(destroyer.getOccupiedSquares().get(2).getColumn(),'B');
+        assertEquals(destroyer.getOccupiedSquares().get(2).getRow(),4);
+        assertEquals(destroyer.getOccupiedSquares().size(),3);
+    }
+
+    @Test
+    public void testSetOccupiedSquares_DESTROYER_Horizontal(){
+        Ship destroyer = new Ship("DESTROYER");
+        destroyer.setOccupiedSquares(2,'B',false);
+        assertEquals(destroyer.getOccupiedSquares().get(0).getColumn(),'B');
+        assertEquals(destroyer.getOccupiedSquares().get(0).getRow(),2);
+        assertEquals(destroyer.getOccupiedSquares().get(2).getColumn(),'D');
+        assertEquals(destroyer.getOccupiedSquares().get(2).getRow(),2);
+        assertEquals(destroyer.getOccupiedSquares().size(),3);
+    }
+
+    @Test
+    public void testSetOccupiedSquares_BATTLESHIP_Vertical(){
+        Ship battleship = new Ship("BATTLESHIP");
+        battleship.setOccupiedSquares(2,'B',true);
+        assertEquals(battleship.getOccupiedSquares().get(0).getColumn(),'B');
+        assertEquals(battleship.getOccupiedSquares().get(0).getRow(),2);
+        assertEquals(battleship.getOccupiedSquares().get(3).getColumn(),'B');
+        assertEquals(battleship.getOccupiedSquares().get(3).getRow(),5);
+        assertEquals(battleship.getOccupiedSquares().size(),4);
+    }
+
+    @Test
+    public void testSetOccupiedSquares_BATTLESHIP_Horizontal(){
+        Ship battleship = new Ship("BATTLESHIP");
+        battleship.setOccupiedSquares(2,'B',false);
+        assertEquals(battleship.getOccupiedSquares().get(0).getColumn(),'B');
+        assertEquals(battleship.getOccupiedSquares().get(0).getRow(),2);
+        assertEquals(battleship.getOccupiedSquares().get(3).getColumn(),'E');
+        assertEquals(battleship.getOccupiedSquares().get(3).getRow(),2);
+        assertEquals(battleship.getOccupiedSquares().size(),4);
+    }
+
+    @Test
+    public void testSetOccupiedSquares_BlankShip(){
+        Ship empty = new Ship();
+        empty.setOccupiedSquares(2,'B',false);
+        assertTrue(empty.getOccupiedSquares().isEmpty());
     }
 }
