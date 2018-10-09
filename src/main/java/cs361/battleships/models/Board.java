0c50b1904330a52daa79adba0a66e8b043e97356
isvpursuit <34354663+isvpursuit@users.noreply.github.com>
2018-10-08 20:53:18 -0700

    Partial Implement of Board.placeShip, Board.attack
    Implemented placeShip and attack partially, need ship class finished for surther implementation.

--- src/main/java/cs361/battleships/models/Board.java
+++ src/main/java/cs361/battleships/models/Board.java
@@ -5,6 +5,9 @@ import java.util.List;
 
 public class Board {
 
+	private List<Ship> ships;
+	private List<Result> attacks;
+
 	/*
 	DO NOT change the signature of this method. It is used by the grading scripts.
 	 */
@@ -16,7 +19,22 @@ public class Board {
 	DO NOT change the signature of this method. It is used by the grading scripts.
 	 */
 	public boolean placeShip(Ship ship, int x, char y, boolean isVertical) {
-		// TODO Implement
+		//Need to implement bound validation, waiting on Ship class
+
+		//validation that ship does not conflict with other ships
+		// ***Needs to be modified for all squares occupied by ship
+		for (Ship allShips: ships) {
+			List<Square> occupied = allShips.getOccupiedSquares();
+			for (Square occu: occupied) {
+				if (occu.getRow() == x && occu.getColumn() == y) {
+					return false;
+				}
+			}
+		}
+
+		//store ship in list of ships
+		ships.add(ship);
+
 		return false;
 	}
 
@@ -24,8 +42,43 @@ public class Board {
 	DO NOT change the signature of this method. It is used by the grading scripts.
 	 */
 	public Result attack(int x, char y) {
-		//TODO Implement
-		return null;
+
+		//set Variables
+		Square location = new Square(x, y);
+		Result atackResult = new Result();
+
+		//check bounds
+		if (x <= 1 || x >= 10 || y <= 'a' || y >= 'j') {
+			atackResult.setResult(AtackStatus.INVALID);
+			return atackResult;
+		}
+
+		//ensure attack location has not been made before
+		for (Result allResults: attacks) {
+			if (allResults.getLocation().getColumn() == location.getColumn() && allResults.getLocation().getRow() == location.getRow()) {
+				atackResult.setResult(AtackStatus.INVALID);
+				return atackResult;
+			}
+		}
+
+		//Validated that result is not INVALID
+		atackResult.setLocation(location);
+
+		//Check for ship at attack location
+		// ***this currently only checks for hit and not sunk or surrender
+		for (Ship ship: ships) {
+			List<Square> occupied = ship.getOccupiedSquares();
+			for (Square occu: occupied) {
+				if (occu.getRow() == x && occu.getColumn() == y) {
+					atackResult.setResult(AtackStatus.HIT);
+					//Check if ship is sunk, if yes check if all ships are sunk
+				} else {
+					atackResult.setResult(AtackStatus.MISS);
+				}
+			}
+		}
+		attacks.add(atackResult);
+		return atackResult;
 	}
 
 	public List<Ship> getShips() {
